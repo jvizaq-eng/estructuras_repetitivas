bda21a91602bedc48aff8320be9a8efc061ae25e
Jennifer <jvizaq@unjbg.edu.pe>
2026-07-27 20:26:01 -0500

    Agregar ejercicio 1 de estructuras repetitivas (version beta)

---
+++ ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+    int contador = 0;
+    cout << "Ingrese un numero (0 para terminar): ";
+    cin >> numero;
+    while (numero != 0) {
+        contador++;
+        cout << "Ingrese otro numero (0 para terminar): ";
+        cin >> numero;
+    }
+    cout << "Ha ingresado " << contador << " numeros." << endl;
+    return 0;
+}
