ddbed34e78e7d68912542bad9012e3e140110a00
Jennifer <jvizaq@unjbg.edu.pe>
2026-07-27 20:26:54 -0500

    Mejora los mensajes de entrada y salida en consola

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     int numero;
     int contador = 0;
+    cout << "=== CONTADOR DE NUMEROS CON BUCLE WHILE ===" << endl;
     cout << "Ingrese un numero (0 para terminar): ";
     cin >> numero;
     while (numero != 0) {
@@ -11,6 +12,7 @@ int main() {
         cout << "Ingrese otro numero (0 para terminar): ";
         cin >> numero;
     }
-    cout << "Ha ingresado " << contador << " numeros." << endl;
+    cout << "===========================================" << endl;
+    cout << "Ha ingresado un total de " << contador << " numeros." << endl;
     return 0;
 }
